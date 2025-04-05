1f9947a171ba74d9c94c824440fd4c36ccf9d695
aj <avetturi@andrew.cmu.edu>
2025-04-05 12:14:40 -0400

    JAX and Landau implemented and verified against C++ implementation

--- Examples/Landau/get_images.py
+++ Examples/Landau/get_images.py
@@ -8,8 +8,11 @@ energy models, enabling a variety of ML methodologies.
 """
 from pfm.analysis import plot_all, animate
 
-files = [r'../Landau/init_0.dat', r'../Landau/last_0.dat']
+files1 = [r'./jax_CH/init_0.dat', r'./jax_CH/last_0.dat']
+files2 = [r'./cpp_landau/init_0.dat', r'./cpp_landau/last_0.dat']  # C++ Output files
 kwargs_list = [{"cmap": "plasma"}, {"cmap": "plasma"}]
 
-#plot_all(files, kwargs_list)
-animate(r'../Landau/trajectory_0.dat', cmap='plasma')
+plot_all(files1, kwargs_list)
+plot_all(files2, kwargs_list)
+#animate(r'/home/aj/GitHub/cahn-hilliard/examples/landau/trajectory_0.dat', cmap='plasma')
+#animate(r'../Landau/trajectory_0.dat', cmap='plasma')
